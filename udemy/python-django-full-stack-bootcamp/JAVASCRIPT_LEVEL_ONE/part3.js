65c9441b3c8e9da99a358c0f1262b29b3ef56f8e
Alex Esakof <aesakof@gmail.com>
2019-12-23 17:08:14 -0500

    Javascript Level One, Part 3 Exercise

---
+++ udemy/python-django-full-stack-bootcamp/JAVASCRIPT_LEVEL_ONE/part3.js
@@ -0,0 +1,7 @@
+var pounds = prompt("Please enter the number of pounds you would like to convert")
+
+kilograms = pounds*0.454
+
+alert(pounds + " pound converts to " + kilograms + " kilograms")
+
+console.log("Conversion Completed")
