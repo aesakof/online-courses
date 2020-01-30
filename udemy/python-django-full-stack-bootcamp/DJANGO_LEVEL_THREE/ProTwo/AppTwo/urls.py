6150cece414925f33756b7f2a38c34051f6dec7b
Alex Esakof <aesakof@gmail.com>
2020-01-29 20:39:35 -0500

    Django Level Three, ModelForms

---
+++ udemy/python-django-full-stack-bootcamp/DJANGO_LEVEL_THREE/ProTwo/AppTwo/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from AppTwo import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('users/', views.users, name='users'),
+]
