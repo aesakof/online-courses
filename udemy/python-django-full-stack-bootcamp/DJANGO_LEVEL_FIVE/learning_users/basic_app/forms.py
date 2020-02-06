b72f76b03b427acc1208c64ab26a01599e14054e
Alex Esakof <aesakof@gmail.com>
2020-02-05 20:59:28 -0500

    Django Level Five (3)

--- udemy/python-django-full-stack-bootcamp/DJANGO_LEVEL_FIVE/learning_users/basic_app/forms.py
+++ udemy/python-django-full-stack-bootcamp/DJANGO_LEVEL_FIVE/learning_users/basic_app/forms.py
@@ -10,7 +10,7 @@ class UserForm(forms.ModelForm):
         fields = ('username','email','password')
 
 
-class UserProfileInfoForms(forms.ModelForm):
+class UserProfileInfoForm(forms.ModelForm):
     class Meta():
         model = UserProfileInfo
         fields = ('portfolio_site','profile_pic')
