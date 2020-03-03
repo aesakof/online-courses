a10bb1702f4e65747649f7027320996a565e9a18
Alex Esakof <aesakof@gmail.com>
2020-03-03 07:04:05 -0500

    Social Media Clone Project (3)

--- udemy/python-django-full-stack-bootcamp/SOCIAL_MEDIA_CLONE_PROJECT/simplesocial/groups/admin.py
+++ udemy/python-django-full-stack-bootcamp/SOCIAL_MEDIA_CLONE_PROJECT/simplesocial/groups/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . imports models
 # Register your models here.
+
+class GroupMemberInline(admin.TabularInline):
+    model = models.GroupMember
+
+admin.site.register(models.Group)
--- udemy/python-django-full-stack-bootcamp/SOCIAL_MEDIA_CLONE_PROJECT/simplesocial/posts/views.py
+++ udemy/python-django-full-stack-bootcamp/SOCIAL_MEDIA_CLONE_PROJECT/simplesocial/posts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.http import Http404
+from django.views import generic
 
+# pip install django-braces
+from braces.views import SelectRelatedMixin
+
+from . import forms
+from . import models
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
 # Create your views here.
